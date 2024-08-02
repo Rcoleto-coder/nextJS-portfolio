9e78cf262c409aec0c7f812a774f8d75d6f85e3d
Rafael Coleto <coleto.rafael@gmail.com>
2024-08-02 11:17:23 -0400

    initial commit

---
+++ nextjs-portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+            <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello I'm Rafael
+            </h1>
+            <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eos
+            minima nihil possimus aliquid dolore similique exercitationem tempora
+            sint. Beatae, illo iusto id explicabo provident numquam obcaecati at
+            totam sint.
+            </p>
+        </div>
+        
+        <div className="col-span-5">
+            <Image 
+                src="/img/168484.jpg"
+                alt="Profile picture"
+                width={300}
+                height={300}
+            />
+           
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
